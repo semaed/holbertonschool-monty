730b278c5e260a526530ffbb3e7c1c88c94e9529
semaed <figsaned@gmail.com>
2023-05-12 16:19:13 +0000

    Add a header file

---
+++ monty.h
@@ -0,0 +1,59 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+int n;
+struct stack_s *prev;
+struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+
+typedef struct instruction_s
+{
+char *opcode;
+void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct global_s - Global variables for the Monty interpreter
+ * @file: File pointer for the Monty bytecode file
+ * @buffer: Buffer for reading lines from the file
+ * @stack: Pointer to the top of the stack
+ * @line_number: Current line number being processed
+ *
+ * Description: A structure to hold global variables
+ */
+
+typedef struct global_s
+{
+char *argument;
+} global_t;
+
+extern global_t *global_variable;
+
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+
+#endif
