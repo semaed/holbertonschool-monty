17194874749e4894bcc2dcfbc4e31bc4ef44b9f7
semaed <figsaned@gmail.com>
2023-05-13 15:42:04 +0000

    Add pop prototype to the monty header file

--- monty.h
+++ monty.h
@@ -57,5 +57,6 @@ void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
 void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 
 #endif
